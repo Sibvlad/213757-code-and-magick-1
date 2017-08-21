8dfcb0117dff6c82ca1c0be0567c185b00838eff
Владислав Сибирев <sibvlad88@mail.ru>
2017-08-21 15:31:41 +0300

    Исправил кавычки

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
   ctx.fillStyle = 'white';
